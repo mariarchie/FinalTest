6ef734a22fcd34fcc30671ac47e68a31c2a30c08
Marianna <mariannadek@gmail.com>
2021-12-29 13:44:10 +0300

    Task solution added

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = new int[8];
+string EvenNumbers(int[] array)
+{
+    string arr = string.Empty;
+    string arr1 = string.Empty;
+    
+    for (int m = 0; m < array.Length; m++)
+    {
+        array[m] = new Random().Next(-10, 11);
+    }
+    for (int m = 0; m < array.Length; m++)
+    {
+        arr += $"{array[m]}  ";
+    }
+    for (int m = 0; m < array.Length; m++)
+    {
+        if (array[m] % 2 == 0)
+        {
+            arr1 += $"{array[m]}  ";
+        }
+    }
+    return $"[{arr}] -> [{arr1}]";
+}
+Console.WriteLine(EvenNumbers(array));
